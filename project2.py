f0d80f7dafc2ab37a506bc89e99ebd9e393828a5
uthragirimuthu <103805640+uthragirimuthu@users.noreply.github.com>
2025-01-23 17:55:43 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,132 @@
+import speech_recognition as sr
+import pyttsx3
+import os
+import datetime
+import subprocess
+import sys
+import pywhatkit 
+import wikipedia
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+recognizer  = sr.Recognizer() 
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def open_software(software_name):
+    if 'chrome' in software_name:
+        speak('Opening Chrome...')
+        program = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+        subprocess.Popen([program])
+
+    elif 'microsoft edge' in software_name:
+        speak('Opening Microsoft Edge...')
+        program = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+        subprocess.Popen([program])
+
+    elif 'play' in software_name:
+        b='Opening Youtube'
+        engine.say(b)
+        engine.runAndWait()
+        pywhatkit.playonyt(software_name)
+
+    elif 'wikipedia' in software_name:
+        b='searching wikipedia'
+        engine.say(b)
+        engine.runAndWait()
+        wikipedia.summary(software_name)   
+
+    elif 'notepad' in software_name:
+        speak('Opening Notepad...')
+        subprocess.Popen(['notepad.exe']) 
+    elif 'calculator' in software_name:
+        speak('Opening Calculator...')
+        subprocess.Popen(['calc.exe'])
+    else:
+        speak(f"I couldn't find the software {software_name}")
+
+def close_software(software_name):
+    if 'chrome' in software_name:
+        speak('Closing Chrome...')
+        os.system("taskkill /f /im chrome.exe")
+
+    elif 'microsoft edge' in software_name:
+        speak('Closing Microsoft Edge...')
+        os.system("taskkill /f /im msedge.exe")
+
+    elif 'notepad' in software_name:
+        speak('Closing Notepad...')
+        os.system("taskkill /f /im notepad.exe")
+    elif 'calculator' in software_name:
+        speak('Closing Calculator...')
+        os.system("taskkill /f /im calculator.exe")
+
+    else:
+        speak(f"I couldn't find any open software named {software_name}")
+    
+
+
+def listen_for_wake_word():
+    with sr.Microphone() as source:
+        print('Listening for wake word...')
+        while True:
+            recognizer.adjust_for_ambient_noise(source, duration=0.5)
+            recorded_audio = recognizer.listen(source)
+            try:
+                text = recognizer.recognize_google(recorded_audio, language='en_US')
+                text = text.lower()
+                if 'vishnu' in text:
+                    print('Wake word detected!')
+                    speak('Hi Sir, How can I help you?')
+                    return True
+            except Exception as ex:
+                print("Could not understand audio, please try again.")
+
+def cmd():
+    with sr.Microphone() as source:
+        print('Clearing background noise... please wait!')
+        recognizer.adjust_for_ambient_noise(source, duration=0.5)
+        print('Ask me anything...')
+        recorded_audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(recorded_audio, language='en_US')
+        text = text.lower()
+        print('Your message:', text)
+    except Exception as ex:
+        print(ex)
+        return
+
+    if 'stop' in text:
+        speak('Stopping the program. Goodbye!')
+        sys.exit()
+    if 'open' in text:
+        software_name = text.replace('open', '').strip()
+        open_software(software_name)
+    elif 'close' in text:
+        software_name = text.replace('close', '').strip()
+        close_software(software_name)
+    elif 'time' in text:
+        current_time = datetime.datetime.now().strftime('%I:%M %p')
+        print(current_time)
+        speak(current_time)
+    elif 'who is god' in text:
+        speak('Ajitheyyy Kadavuleyy')
+    elif 'who are you' in text:
+        speak('i am vishnu')
+    elif 'what is your name' in text:
+        speak('My name is vishwa Your Artificial Intelligence')
+    elif 'how are you' in text:
+        speak('I am fine')
+    elif 'i love you' in text:
+        speak('same too you')
+
+while True:
+    if listen_for_wake_word():
+        while True: 
+            if cmd():
+                break
